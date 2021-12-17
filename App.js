6cdb2af2e090150b368fa87effc724842a8a72b1
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-12-18 00:24:57 +0600

    loadd data

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const API_KEY = '4b36ed411e77940a73b1838112abef3f';
+
 export default function App() {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+
+
+  async function fetchWeeatherData(cityName) {
+    setLoaded(false);
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    console.log(API);
+    try {
+      const response = await fetch(API);
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log(data);
+        setWeatherData(data);
+      }
+      else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeeatherData('Mumbai');
+    console.log(weatherData);
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>length: {weatherData?.name}</Text>
+
     </View>
   );
 }
